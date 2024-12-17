8738e7cdedd2bd130fac1ba26af4f0748c4677fb
SafaMaqbool <s.maq.basha@gmail.com>
2024-12-18 00:26:01 +0500

    infinite scroll applied to logos

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,17 +1,30 @@
-import acmeLogo from '@/assets/logo-acme.png'
+"use client";
+import acmeLogo from "@/assets/logo-acme.png";
 import quantum from "@/assets/logo-quantum.png";
 import echoLogo from "@/assets/logo-echo.png";
-import pulseLogo from '@/assets/logo-pulse.png'
-import celestialLogo from '@/assets/logo-celestial.png'
-import apexLogo from '@/assets/logo-apex.png'
-import Image from 'next/image';
+import pulseLogo from "@/assets/logo-pulse.png";
+import celestialLogo from "@/assets/logo-celestial.png";
+import apexLogo from "@/assets/logo-apex.png";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 export const LogoTicker = () => {
   return (
-    <div className='py-8 bg-white md:py-12'>
+    <div className="py-8 bg-white md:py-12">
       <div className="container ">
-        <div className='flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]'>
-          <div className="flex gap-14 flex-none">
+        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <motion.div className="flex gap-14 flex-none pr-14" animate={
+            {
+              translateX: "-50%",
+            }
+          }
+          transition={{
+            duration:20,
+            repeat: Infinity,
+            ease: "linear",
+            repeatType: "loop"
+          }}
+          >
             <Image
               src={acmeLogo}
               alt="acme logo"
@@ -42,7 +55,38 @@ export const LogoTicker = () => {
               alt="apex logo"
               className="logo-ticker-image"
             />
-          </div>
+            {/* second set of logos for infinite animate illusion */}
+            <Image
+              src={acmeLogo}
+              alt="acme logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={quantum}
+              alt="quantun logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={echoLogo}
+              alt="echo logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={pulseLogo}
+              alt="pulse logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={celestialLogo}
+              alt="celestial logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={apexLogo}
+              alt="apex logo"
+              className="logo-ticker-image"
+            />
+          </motion.div>
         </div>
       </div>
     </div>
