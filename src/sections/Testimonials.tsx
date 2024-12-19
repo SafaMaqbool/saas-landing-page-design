e066ca2c14b8bed782e255942a8c08344586f2c2
SafaMaqbool <s.maq.basha@gmail.com>
2024-12-19 20:59:22 +0500

    testimonial section animated

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,3 +1,4 @@
+'use client';
 import avatar1 from "@/assets/avatar-1.png";
 import avatar2 from "@/assets/avatar-2.png";
 import avatar3 from "@/assets/avatar-3.png";
@@ -8,7 +9,9 @@ import avatar7 from "@/assets/avatar-7.png";
 import avatar8 from "@/assets/avatar-8.png";
 import avatar9 from "@/assets/avatar-9.png";
 import Image from "next/image";
-import { twMerge } from "tailwind-merge";
+
+import { motion } from "framer-motion";
+import React from "react";
 
 const testimonials = [
   {
@@ -71,26 +74,48 @@ const firstColumn = testimonials.slice(0, 3);
 const secondColumn = testimonials.slice(3, 6);
 const thirdColumn = testimonials.slice(6, 9);
 
-const TestimonialsCol = (props: {className?: string;testimonials:typeof testimonials}) => (
-  <div className={twMerge("flex flex-col gap-6 mt-10 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)]",props.className)}>
-    {props.testimonials.map(({ text, imageSrc, name, username }) => (
-      <div className="card">
-        <div>{text}</div>
-        <div className="flex items-center gap-2 mt-5">
-          <Image
-            src={imageSrc}
-            width={40}
-            height={40}
-            alt={name}
-            className="h-10 w-10 rounded-full"
-          />
-          <div className="flex flex-col ">
-            <div className="font-medium tracking-tight leading-5">{name}</div>
-            <div className="leading-5 tracking-tight">{username}</div>
-          </div>
-        </div>
-      </div>
-    ))}
+const TestimonialsCol = (props: {
+  className?: string;
+  testimonials: typeof testimonials;
+  duration?:number;
+}) => (
+  <div className={props.className}>
+    <motion.div
+    animate={{
+      translateY:'-50%'
+    }}
+    transition={{
+      repeat:Infinity,
+      ease:'linear',
+      repeatType:'loop',
+      duration:props.duration || 10
+    }}
+    className="flex flex-col gap-6 -translate-y-1/2 pb-6">
+      {[...new Array(2)].fill(0).map((_, index) => (
+        <React.Fragment key={index}>
+          {props.testimonials.map(({ text, imageSrc, name, username }) => (
+            <div className="card">
+              <div>{text}</div>
+              <div className="flex items-center gap-2 mt-5">
+                <Image
+                  src={imageSrc}
+                  width={40}
+                  height={40}
+                  alt={name}
+                  className="h-10 w-10 rounded-full"
+                />
+                <div className="flex flex-col ">
+                  <div className="font-medium tracking-tight leading-5">
+                    {name}
+                  </div>
+                  <div className="leading-5 tracking-tight">{username}</div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </React.Fragment>
+      ))}
+    </motion.div>
   </div>
 );
 export const Testimonials = () => {
@@ -107,15 +132,15 @@ export const Testimonials = () => {
             essential tool for users aroud the world{" "}
           </p>
         </div>
-        <div className="flex justify-center gap-6">
-          <TestimonialsCol testimonials={firstColumn} />
+        <div className="flex justify-center gap-6 [mask-image:linear-gradient(to_bottom,transparent,black_25%,black_75%,transparent)] max-h-[738px] overflow-hidden mt-10 ">
+          <TestimonialsCol testimonials={firstColumn} duration={15} />
           <TestimonialsCol
-            testimonials={secondColumn}
-            className="hidden md:flex"
+            testimonials={secondColumn} duration={19}
+            className="hidden md:block"
           />
           <TestimonialsCol
-            testimonials={thirdColumn}
-            className="hidden lg:flex"
+            testimonials={thirdColumn} duration={17}
+            className="hidden lg:block"
           />
         </div>
       </div>
